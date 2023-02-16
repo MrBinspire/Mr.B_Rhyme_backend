df07e072ba579d57fb3d878c69d88186a2b612c8
MrBinspire <mrbinspire@gmail.com>
2023-02-16 13:40:02 +0530

    changes for static files1

--- blake_dict/settings.py
+++ blake_dict/settings.py
@@ -153,7 +153,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
+import os
 STATIC_URL = "static/"
 STATICFILES_DIRS = ['static_files']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
