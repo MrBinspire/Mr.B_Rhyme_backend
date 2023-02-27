0e41c43e0c4c48134e2b15a73bd9f0e1ac750a57
MrBinspire <mrbinspire@gmail.com>
2023-02-27 17:12:07 +0530

    search bar in django admin

--- home/admin.py
+++ home/admin.py
@@ -4,6 +4,7 @@ from .models import *
 
 class RhymesAdmin(admin.ModelAdmin):
     list_display = ("id", "user", "word", "Word_of_the_day")
+    search_fields = ('word',)
 
 
 class WordOfTheDayAdmin(admin.ModelAdmin):
@@ -12,10 +13,12 @@ class WordOfTheDayAdmin(admin.ModelAdmin):
 
 class AcceptedAdmin(admin.ModelAdmin):
     list_display = ("id", "date", "user", "word", "Word_of_the_day", "count")
+    search_fields = ('word',)
 
 
 class RejectedAdmin(admin.ModelAdmin):
     list_display = ("id", "date", "user", "word", "Word_of_the_day", "count")
+    search_fields = ('word',)
 
 
 admin.site.register(Rhymes, RhymesAdmin)
